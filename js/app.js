6699e8a0ef42bf83cc2bb18e2df9081f1056af3d
jborruel308@me.com <jborruel308@me.com>
2024-07-30 17:46:27 -0700

    variables and cached elements added

---
+++ js/app.js
@@ -0,0 +1,42 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+let board = '';
+let turn = '';
+let winner = '';
+let tie = '';
+
+/*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelectorAll ('.sqr');
+const  messageEl = document.querySelector ('#message');
+
+console.log(squareEls)
+console.log(messageEl)
+/*-------------------------------- Functions --------------------------------*/
+
+const init = () => {
+    console.log ()
+};
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+//1) Define the required variables used to track the state of the game.
+
+//2) Store cached element references.
+
+//3) Upon loading, the game state should be initialized, and a function should 
+//   be called to render this game state.
+
+//4) The state of the game should be rendered to the user.
+
+//5) Define the required constants.
+
+//6) Handle a player clicking a square with a `handleClick` function.
+
+//7) Create Reset functionality.
